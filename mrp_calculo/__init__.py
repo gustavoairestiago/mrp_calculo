5abac3f140efcbe49fee5d13898a4dde6d37722e
Gustavo Aires Tiago <gustavoairestiago@gmail.com>
2025-09-23 16:53:19 -0300

    Update __init__.py

--- mrp_calculo/__init__.py
+++ mrp_calculo/__init__.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
-from .core import ler_arquivos, aplicar_mapeamento, calcular_indicadores, executar
-__all__ = ["ler_arquivos", "aplicar_mapeamento", "calcular_indicadores", "executar"]
+from .core import ler_arquivos, aplicar_mapeamento, calcular_indicadores, executar, extrair_notas
+__all__ = ["ler_arquivos", "aplicar_mapeamento", "calcular_indicadores", "executar", "extrair_notas"]
